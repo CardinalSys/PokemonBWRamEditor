9b96ccc518aa0e8af74b8fbb8af29513feb81665
Cardinal <alessandro.benvenuti09@gmail.com>
2024-02-20 21:49:04 +0100

    Read Sprites RAM Location
    -Get a List with all the sprites
    -Show image of the current active sprites (Only player, cheren and bianca)

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,8 +1,14 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using Avalonia.Platform.Storage;
+using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 
 namespace PkmBWRamEditor
 {
@@ -35,10 +41,51 @@ namespace PkmBWRamEditor
 			}
 		}
 
-		private void SpritesClick(object sender, RoutedEventArgs e)
+		private async void SpritesClick(object sender, RoutedEventArgs e)
 		{
 			MainPanel.IsVisible = false;
 			SpritesPanel.IsVisible = true;
+
+			RamAPI ram = RamAPI.GetInstance();
+
+			ram.ProcName = processName;
+
+			ram.spritesMemoryLocation.Clear();
+
+			ram.ReadPos(await ram.GetBaseSpriteAddress());
+
+			try
+			{
+				var ImageToView = new Bitmap("C:\\Users\\tmkuh\\source\\repos\\PkmBWRamEditor\\PkmBWRamEditor\\img\\" + ram.spritesMemoryLocation[0][240].ToString("X") + ".png");
+				Sprite1.Source = ImageToView;
+			} catch { }
+
+			try
+			{
+				var ImageToView2 = new Bitmap("C:\\Users\\tmkuh\\source\\repos\\PkmBWRamEditor\\PkmBWRamEditor\\img\\" + ram.spritesMemoryLocation[1][240].ToString("X") + ".png");
+				Sprite2.Source = ImageToView2;
+			}
+			catch { }
+
+			try
+			{
+				var ImageToView3 = new Bitmap("C:\\Users\\tmkuh\\source\\repos\\PkmBWRamEditor\\PkmBWRamEditor\\img\\" + ram.spritesMemoryLocation[2][240].ToString("X") + ".png");
+				Sprite3.Source = ImageToView3;
+			}
+			catch { }
+
+			try
+			{
+				var ImageToView4 = new Bitmap("C:\\Users\\tmkuh\\source\\repos\\PkmBWRamEditor\\PkmBWRamEditor\\img\\" + ram.spritesMemoryLocation[3][240].ToString("X") + ".png");
+				Sprite4.Source = ImageToView4;
+			} catch { }
+
+
+			
+			
+			
+			
+
 		}
 
 
---
+++ RamAPI.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Memory;
+
+namespace PkmBWRamEditor
+{
+	internal class RamAPI
+	{
+
+		private static RamAPI _instance;
+
+        public string ProcName { get; set; }
+        private string _baseSpritesAddress = "?? ?? 3? 02 90 21 25 02 ?? 5? 1D 02 ?? 86 1D 02";
+		private Mem _procMem = new Mem();
+
+		public List<byte[]> spritesMemoryLocation = new List<byte[]>();
+
+		public static RamAPI GetInstance()
+		{
+			if (_instance == null)
+				_instance = new RamAPI();
+
+			return _instance;
+		}
+
+		public async Task<string> GetBaseSpriteAddress() 
+		{
+			ProcName = "DeSmuME_0.9.13_x64.exe";
+			if (_procMem.OpenProcess(ProcName))
+			{
+				IEnumerable<long> AoBScanResults = await _procMem.AoBScan(_baseSpritesAddress, true, true, true);
+
+				UpdateSpriteList(AoBScanResults.Last().ToString());
+
+				return AoBScanResults.Last().ToString("X");
+			}
+
+			return "Null";
+		}
+
+		public void UpdateSpriteList(string lastAddress)
+		{
+			long address = long.Parse(lastAddress);
+
+			while(_procMem.ReadByte(address.ToString("X")) != 0xFF)
+			{
+				spritesMemoryLocation.Add(_procMem.ReadBytes((address - 144).ToString("X"), 256));
+
+				address -= 256;
+			}
+
+			spritesMemoryLocation.RemoveAt(spritesMemoryLocation.Count - 1);
+		}
+
+		public int ReadPos(string address)
+		{
+			return _procMem.ReadByte(address);
+		}
+	}
+}
